6d556b4153bf5f9aad7658414df86666d78e79f2
Abhinav Sharma <abhi18av@outlook.com>
2019-06-24 01:41:15 +0530

    pushing on the html files now

--- src/io/perun/example/stylesheet.clj
+++
@@ -1,9 +0,0 @@
-(ns io.perun.example.stylesheet
-  (:require [garden.def :as gdn]))
-
-(gdn/defstyles screen
-  [:body
-   {:font-family "Helvetica Neue"
-    :font-size   "16px"
-    :line-height 1.5}])
-
