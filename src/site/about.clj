44119645a29a3ce309647cc0efc1bf4bdb2b83d9
Abhinav Sharma <abhi18av@gmail.com>
2019-07-07 00:09:40 +0530

    about page is almost done now

--- src/site/about.clj
+++ src/site/about.clj
@@ -1,13 +1,26 @@
 (ns site.about
   (:require [hiccup.page :refer [html5]]))
 
-
 (defn render [{global-meta :meta posts :entries}]
   (html5 {:lang "en" :itemtype "http://schema.org/Blog"}
     [:head
       [:title (:site-title global-meta)]
       [:meta {:charset "utf-8"}]
       [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]]
-    [:body
-     [:p "This is a demonstration of a static page, for content that won't change"]]))
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}
+         [:body
+          [:img {:src "./assets/images/profile.jpg", :alt "Profile Image"}]
+          [:p " I am not from Krypton, or Stark Tower or even BatCave )"]
+          [:p " What am I passionate about, you ask? "]
+          [:p " I am passionate about Startups "]
+          [:p " My favourite hobby, my drive, is Growth-hacking! "]
+          [:h2 "Skills"]
+          [:h4 " Have programming experience with "]
+          [:ul {:class "skill-list"}
+           [:li "Python"]
+           [:li "Go"]
+           [:li "Clojure"]
+           [:li "JavaScript and TypeScript"]
+           [:li "ClojureScript ( hosted on NodeJS )"]
+           [:li "ReactJS"]
+           [:li "MySQL - MongoDB - SQLite3 "]]]]]))
