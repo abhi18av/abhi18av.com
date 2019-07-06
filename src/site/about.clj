cc86b6be65103540943b2220d63ebb0be5af77be
Abhinav Sharma <abhi18av@gmail.com>
2019-07-07 00:41:03 +0530

    working on the about me page

--- src/site/about.clj
+++ src/site/about.clj
@@ -1,16 +1,18 @@
 (ns site.about
   (:require [hiccup.page :refer [html5]]))
 
+
 (defn render [{global-meta :meta posts :entries}]
   (html5 {:lang "en" :itemtype "http://schema.org/Blog"}
-    [:head
-      [:title (:site-title global-meta)]
-      [:meta {:charset "utf-8"}]
-      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}
+         [:head
+          [:title (:site-title global-meta)]
+          [:meta {:charset "utf-8"}]
+          [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+          [:link {:rel "stylesheet" :type "text/css" :href "style.css"}]]
          [:body
           [:img {:src "./assets/images/profile.jpg", :alt "Profile Image"}]
-          [:p " I am not from Krypton, or Stark Tower or even BatCave )"]
+          [:p " I am not from Krypton, or Stark Tower or even BatCave ;)"]
           [:p " What am I passionate about, you ask? "]
           [:p " I am passionate about Startups "]
           [:p " My favourite hobby, my drive, is Growth-hacking! "]
@@ -23,4 +25,5 @@
            [:li "JavaScript and TypeScript"]
            [:li "ClojureScript ( hosted on NodeJS )"]
            [:li "ReactJS"]
-           [:li "MySQL - MongoDB - SQLite3 "]]]]]))
+           [:li "MySQL - MongoDB - SQLite3 "]]]
+         [:footer ""]))
