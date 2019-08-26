3e6876ab84bd3f341840ee3f7b2b7071531e2e62
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-26 18:23:16 +0530

    update nuslack project

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,9 +7,11 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/eklavya/projects/portfolio/abhi18av.com/.cache/dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/eklavya/projects/portfolio/abhi18av.com/src/pages/index.js"))
 }
 
 exports.json = {
+  "dev-404-page.json": require("/Users/eklavya/projects/portfolio/abhi18av.com/.cache/json/dev-404-page.json"),
   "index.json": require("/Users/eklavya/projects/portfolio/abhi18av.com/.cache/json/index.json")
 }
