1fabef1ea3b917215af402741238eb2714c295a2
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-08-26 18:08:36 +0530

    testing deployment

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,10 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/eklavya/projects/portfolio/abhi18av.com/.cache/dev-404-page.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/eklavya/projects/portfolio/abhi18av.com/src/pages/index.js")
 }
 
 exports.json = {
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/eklavya/projects/portfolio/abhi18av.com/.cache/json/dev-404-page.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/eklavya/projects/portfolio/abhi18av.com/.cache/json/index.json")
 }
 
